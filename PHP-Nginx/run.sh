2077ce19961453b0b7cf9c84aebeca7a8b9d0974
Dante Ulises Rangel Robles <dante.ulises@red-wolf.com.mx>
2018-10-26 00:59:15 -0500

    Agregando run en puerto y variables de entorno al instancear la imagen

--- PHP-Nginx/run.sh
+++ PHP-Nginx/run.sh
@@ -8,3 +8,5 @@ docker rm -f php_nginx
 
 
 docker run -d --name php_nginx -p 80:80 -p 443:443 php:nginx
+# docker run -d --name php_nginx -p 80:80 -e "prueba2=4321" php:nginx
+#  docker run -d --name php_nginx_1000 -p 1000:80 php:nginx
